d4f959e987904cdb6e30a911efcb5fd43fd72bc9
Thomas_Talbot <thomastalbot1@Thomas-Talbots-MacBook-Pro.local>
2021-11-20 14:44:10 -0500

    Wrote a program to steer and move car fwd and reverse using bluetooth control.

---
+++ finalProjectDriving.cpp
@@ -0,0 +1,186 @@
+/*******************************************************************************
+* Author: Thomas W.  Talbot 
+* Last Date Modified: 11/20/21
+* Purpose: This program runs on the mbed microcontroller and continually checks
+* the Adafrtuit BLE module for drive commands. 
+*******************************************************************************/
+
+
+#include "mbed.h"
+#include "rtos.h"
+#include "Motor.h"
+
+
+Serial blue(p28, p27); 
+Serial pc(USBTX, USBRX); 
+Motor right(p21, p5, p6); 
+Motor left(p22, p7, p8); 
+DigitalOut led1(LED1); 
+
+volatile float right_speed =  0.0; //absolute value 0-1
+volatile float left_speed = 0.0; //absolute value 0-1 
+volatile bool forward = false; //car starts in reverse gear
+
+/*******************************************************************************
+* speed_control - The purpose of this function is to set the speed on the left 
+* and right DC car motors twice a second. 
+*******************************************************************************/
+void speed_control(void)
+{  
+    while(1)
+    {
+        
+        right.speed(right_speed); 
+        left.speed(left_speed); 
+        
+        Thread::wait(500); 
+    }  
+}
+    
+
+int main() {
+    Thread t1(speed_control); 
+    char bnum =0; 
+    char bhit =0; 
+    while(1) 
+    {
+        //led1 = !1ed1; //heartbeat
+        //led1 = 0; 
+       // Thread::wait(200); 
+        //led1 = 1; 
+        //Thread::wait(200); 
+        if(blue.readable() && blue.getc() == '!')
+        {
+            pc.printf("Bluetooth is readable\n\r"); 
+            if(blue.getc() == 'B')
+            {
+                
+                //recieved a button data packet
+                bnum = blue.getc(); 
+                bhit = blue.getc(); 
+                if (blue.getc()==char(~('!' + 'B' + bnum + bhit))) { //checksum OK?
+                    switch(bnum)
+                    {
+                        
+                        case '5':
+                            //Up arrow - increase forward speed 
+                            if(bhit == '1')
+                            {
+                                pc.printf("up arrow hit\n\r"); 
+                                if(!forward)
+                                {
+                                    //just switched into forward gear
+                                    //the car should be stopped
+                                    forward =true; 
+                                    right_speed = 0.0; 
+                                    left_speed = 0.0; 
+                                }
+                                else
+                                {
+                                    if(right_speed <1.0)
+                                    {
+                                        //pc.printf("adding to the right speed\n\r"); 
+                                        right_speed = right_speed + 0.1; 
+                                        //pc.printf("%f\n\r", right_speed); 
+                                    }
+                                    if(left_speed <1.0)
+                                    {
+                                        //pc.printf("Adding to the left speed\n\r"); 
+                                        left_speed = left_speed + 0.1; 
+                                        //pc.printf("%f\n\r", left_speed);
+                                    }
+                                }// end if-else block 
+                            }//end hit if block 
+                            break; 
+                        case '6':
+                             if(bhit == '1')
+                             {
+                                 pc.printf("down arrow hit\n\r");
+                                 
+                                 //Down arrow - throw the car into reverse and increase 
+                                 if(forward) 
+                                 {
+                                     //just moved into reverse gear 
+                                     //the car should be stopped
+                                     forward = false; 
+                                     right_speed = 0.0; 
+                                     left_speed = 0.0; 
+                                 }
+                                 else
+                                 {
+                                     if(right_speed > -1.0)
+                                     {
+                                         right_speed = right_speed -0.1; 
+                                     } 
+                                     if(left_speed > 1.0) 
+                                     {
+                                         left_speed = left_speed - 0.1; 
+                                     } 
+                                }//end the if-else block 
+                            }//end hit if block 
+                            break; 
+                        case '7': 
+                            //left arrow 
+                            if(bhit == '1') 
+                            {
+                                pc.printf("left arrow hit"); 
+                                if(forward)
+                                {
+                                    //set right wheel to 0.5 and 
+                                    //left wheel to -0.5
+                                    right_speed = 0.5; 
+                                    left_speed = -0.5; 
+                                }
+                                else
+                                {
+                                    //turning left in reverse
+                                    right_speed = -0.5; 
+                                    left_speed = 0.5; 
+                                }
+                            }//end bhit if 
+                                    
+                            break; 
+                            
+                        case '8':
+                            //right arrow 
+                            if(bhit == '1') 
+                            {
+                                pc.printf("right arrow hit"); 
+                                if(forward)
+                                {
+                                    //set right wheel to -0.5 and 
+                                    //left wheel to 0.5
+                                    right_speed = -0.5; 
+                                    left_speed = 0.5; 
+                                }
+                                else
+                                {
+                                    //turning right in reverse
+                                    right_speed = 0.5; 
+                                    left_speed = -0.5; 
+                                }
+                            }//end bhit if
+                                
+                            break; 
+                            
+                        default:
+                            break; 
+                    } //end switch statement 
+                }//end if statement for checksum
+            }//end if statement for button
+        }//end  if statement for readable 
+        //Thread::wait(100); 
+    }//end while loop 
+}//end main
+
+
+
+
+
+
+
+
+
+
+
+
