799279f5ca0c8ebef1c156cfce8ba6cc3b3dab73
Helmut Lord <helmutlord@lawn-128-61-6-199.lawn.gatech.edu>
2021-11-22 15:36:14 -0500

    Pin numbers.

---
+++ src/pins.h
@@ -0,0 +1,7 @@
+#DEFINE AUDIO_OUT p18 //AnalogOut
+#DEFINE R_LEFT    p26 //PwmOut
+#DEFINE B_LEFT    p27 //PwmOut
+#DEFINE R_RIGHT   p28 //PwmOut
+#DEFINE R_LEFT    p29 //PwmOut
+#DEFINE G_LEFT    p9  //DigitalOut
+#DEFINE G_RIGHT   p10 //DigitalOut
